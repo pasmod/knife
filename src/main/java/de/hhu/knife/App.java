c6e506d2f71b8a6e33ca022e7d776dc80ee30f4f
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-28 15:37:56 +0200

    added a route for extracting the functions

--- src/main/java/de/hhu/knife/App.java
+++ src/main/java/de/hhu/knife/App.java
@@ -1,14 +1,35 @@
 package de.hhu.knife;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
 
 import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.thoughtworks.qdox.JavaProjectBuilder;
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaMethod;
 
 public class App {
 
 	public static void main(String[] args) throws IOException {
 
-		get("/methods", (req, res) -> "Hello World");
+		// Configure Spark
+		port(4567);
+
+		get("/methods", "application/json", (request, response) -> {
+			final JavaProjectBuilder builder = new JavaProjectBuilder();
+			final String clazz = request.queryParams("class");
+			System.out.println(clazz);
+			builder.addSource(new StringReader(clazz));
+			JavaClass javaClass = builder.getClasses().stream().collect(Collectors.toList()).get(0);
+			List<JavaMethod> javaMethods = javaClass.getMethods();
+			List<String> methods = javaMethods.stream().map(m -> m.getCodeBlock()).collect(Collectors.toList());
+			return methods;
+		});
+		;
 	}
 
 }
