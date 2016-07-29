b1a14295048195c86f0880f65097c2270951ecb2
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-29 16:24:01 +0200

    added exception handling for the case that code is not parsable

--- src/main/java/de/hhu/knife/App.java
+++ src/main/java/de/hhu/knife/App.java
@@ -11,10 +11,12 @@ import java.util.stream.Collectors;
 
 import com.thoughtworks.qdox.JavaProjectBuilder;
 import com.thoughtworks.qdox.model.JavaMethod;
+import com.thoughtworks.qdox.parser.ParseException;
 
 import de.hhu.knife.beans.KJavaClass;
 import de.hhu.knife.beans.KJavaMethod;
 import de.hhu.knife.beans.Segment;
+import de.hhu.knife.beans.State;
 import de.hhu.knife.transformers.JsonTransformer;
 
 public class App {
@@ -26,9 +28,12 @@ public class App {
 
 		post("/extract", "application/json", (request, response) -> {
 			// TODO: It is necessary to init the builder for each request?
-			// TODO: Add exception handling (case: code not parsable)
 			final JavaProjectBuilder builder = new JavaProjectBuilder();
-			builder.addSource(new StringReader(request.queryParams("class")));
+			try {
+				builder.addSource(new StringReader(request.queryParams("class")));
+			} catch (ParseException e) {
+				return new Segment.Builder().state(State.PARSE_ERROR).build();
+			}
 			List<KJavaClass> kJavaClasses = new ArrayList<>();
 			for (List<JavaMethod> javaMethodsList : builder.getClasses().stream().collect(Collectors.toList()).stream()
 					.map(clazz -> clazz.getMethods()).collect(Collectors.toList())) {
--- src/main/java/de/hhu/knife/beans/Segment.java
+++ src/main/java/de/hhu/knife/beans/Segment.java
@@ -5,18 +5,27 @@ import java.util.List;
 public class Segment {
 	@SuppressWarnings("unused")
 	private List<KJavaClass> classes;
+	@SuppressWarnings("unused")
+	private State state;
 
 	public static class Builder {
 		private List<KJavaClass> classes;
+		private State state;
 
 		public Builder classes(List<KJavaClass> classes) {
 			this.classes = classes;
 			return this;
 		}
 
+		public Builder state(State state) {
+			this.state = state;
+			return this;
+		}
+
 		public Segment build() {
 			Segment segment = new Segment();
 			segment.classes = classes;
+			segment.state = state;
 			return segment;
 		}
 	}
