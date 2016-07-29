be7e9e085c7b9c79bd166c62d0f25e2918672c4a
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-29 11:52:02 +0200

    added simple json transformer

---
+++ src/main/java/de/hhu/knife/transformers/JsonTransformer.java
@@ -0,0 +1,16 @@
+package de.hhu.knife.transformers;
+
+import com.google.gson.Gson;
+
+import spark.ResponseTransformer;
+
+public class JsonTransformer implements ResponseTransformer {
+
+	private Gson gson = new Gson();
+
+	@Override
+	public String render(Object model) {
+		return gson.toJson(model);
+	}
+
+}
