fd3c27bf76e2b70b86de40eca7ec3c02df52411a
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-29 11:02:59 +0200

    added some java source code examples

---
+++ examples/example1.java
@@ -0,0 +1,11 @@
+public class Example {
+
+	public static void main(String[] args) {
+		System.out.println(add(2, 3));
+	}
+
+	public static int add(final int a, final int b) {
+		return a + b;
+	}
+
+}
