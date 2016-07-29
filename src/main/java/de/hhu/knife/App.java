d14194504fdd53a28e964745e275f9ca3ecc03de
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-29 10:10:52 +0200

    refactored the /method/blocks path and added some todos

--- src/main/java/de/hhu/knife/App.java
+++ src/main/java/de/hhu/knife/App.java
@@ -1,7 +1,7 @@
 package de.hhu.knife;
 
-import static spark.Spark.get;
 import static spark.Spark.port;
+import static spark.Spark.post;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -9,8 +9,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.thoughtworks.qdox.JavaProjectBuilder;
-import com.thoughtworks.qdox.model.JavaClass;
-import com.thoughtworks.qdox.model.JavaMethod;
 
 public class App {
 
@@ -19,17 +17,15 @@ public class App {
 		// Configure Spark
 		port(4567);
 
-		get("/methods", "application/json", (request, response) -> {
+		post("/method/blocks", (request, response) -> {
+			// TODO: It is necessary to init the builder for each request?
+			// TODO: Add exception handling (case: code not parsable)
 			final JavaProjectBuilder builder = new JavaProjectBuilder();
-			final String clazz = request.queryParams("class");
-			System.out.println(clazz);
-			builder.addSource(new StringReader(clazz));
-			JavaClass javaClass = builder.getClasses().stream().collect(Collectors.toList()).get(0);
-			List<JavaMethod> javaMethods = javaClass.getMethods();
-			List<String> methods = javaMethods.stream().map(m -> m.getCodeBlock()).collect(Collectors.toList());
+			builder.addSource(new StringReader(request.queryParams("class")));
+			List<String> methods = builder.getClasses().stream().collect(Collectors.toList()).get(0).getMethods()
+					.stream().map(m -> m.getCodeBlock()).collect(Collectors.toList());
 			return methods;
 		});
-		;
 	}
 
 }
