b04061830e1c5ac7776b77d31bd5369ef779dc93
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-29 11:52:41 +0200

    main app returns the results as json

--- src/main/java/de/hhu/knife/App.java
+++ src/main/java/de/hhu/knife/App.java
@@ -10,6 +10,8 @@ import java.util.stream.Collectors;
 
 import com.thoughtworks.qdox.JavaProjectBuilder;
 
+import de.hhu.knife.transformers.JsonTransformer;
+
 public class App {
 
 	public static void main(String[] args) throws IOException {
@@ -17,7 +19,7 @@ public class App {
 		// Configure Spark
 		port(4567);
 
-		post("/method/blocks", (request, response) -> {
+		post("/method/blocks", "application/json", (request, response) -> {
 			// TODO: It is necessary to init the builder for each request?
 			// TODO: Add exception handling (case: code not parsable)
 			final JavaProjectBuilder builder = new JavaProjectBuilder();
@@ -25,7 +27,7 @@ public class App {
 			List<String> methods = builder.getClasses().stream().collect(Collectors.toList()).get(0).getMethods()
 					.stream().map(m -> m.getCodeBlock()).collect(Collectors.toList());
 			return methods;
-		});
+		}, new JsonTransformer());
 	}
 
 }
