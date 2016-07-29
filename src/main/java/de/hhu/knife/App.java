9e71588774ff991f352e058157b1707959acd00a
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-29 15:16:59 +0200

    refactored the main app

--- src/main/java/de/hhu/knife/App.java
+++ src/main/java/de/hhu/knife/App.java
@@ -5,12 +5,16 @@ import static spark.Spark.post;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.thoughtworks.qdox.JavaProjectBuilder;
+import com.thoughtworks.qdox.model.JavaMethod;
 
-import de.hhu.knife.beans.MethodBlocks;
+import de.hhu.knife.beans.KJavaClass;
+import de.hhu.knife.beans.KJavaMethod;
+import de.hhu.knife.beans.Segment;
 import de.hhu.knife.transformers.JsonTransformer;
 
 public class App {
@@ -20,14 +24,22 @@ public class App {
 		// Configure Spark
 		port(4567);
 
-		post("/method/blocks", "application/json", (request, response) -> {
+		post("/extract", "application/json", (request, response) -> {
 			// TODO: It is necessary to init the builder for each request?
 			// TODO: Add exception handling (case: code not parsable)
 			final JavaProjectBuilder builder = new JavaProjectBuilder();
 			builder.addSource(new StringReader(request.queryParams("class")));
-			List<String> methodBlocks = builder.getClasses().stream().collect(Collectors.toList()).get(0).getMethods()
-					.stream().map(m -> m.getCodeBlock()).collect(Collectors.toList());
-			return new MethodBlocks(methodBlocks);
+			List<KJavaClass> kJavaClasses = new ArrayList<>();
+			for (List<JavaMethod> javaMethodsList : builder.getClasses().stream().collect(Collectors.toList()).stream()
+					.map(clazz -> clazz.getMethods()).collect(Collectors.toList())) {
+				List<KJavaMethod> kJavaMethods = new ArrayList<>();
+				for (JavaMethod javaMethod : javaMethodsList) {
+					kJavaMethods.add(new KJavaMethod.Builder().codeBlock(javaMethod.getCodeBlock()).build());
+				}
+				kJavaClasses.add(new KJavaClass.Builder().methods(kJavaMethods).build());
+			}
+
+			return new Segment.Builder().classes(kJavaClasses).build();
 		}, new JsonTransformer());
 	}
 
