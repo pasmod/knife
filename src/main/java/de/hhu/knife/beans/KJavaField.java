1c5f1775f5c4300e41b4eab7fcbe5316c1e5f4c7
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-01 12:48:53 +0200

    removed unused imports

--- src/main/java/de/hhu/knife/beans/KJavaField.java
+++ src/main/java/de/hhu/knife/beans/KJavaField.java
@@ -1,20 +1,16 @@
 package de.hhu.knife.beans;
 
-import java.util.List;
 import com.thoughtworks.qdox.model.*;
-
-import de.hhu.knife.beans.KJavaClass.Builder;
 
 public class KJavaField {
 	private String codeBlock;
 	private String type;
 	private String fieldName;
+
 	public static class Builder {
 		private JavaField javaField;
-		
-		
-		public Builder fieldInformation(JavaField javaField)
-		{
+
+		public Builder fieldInformation(JavaField javaField) {
 			this.javaField = javaField;
 			return this;
 		}
