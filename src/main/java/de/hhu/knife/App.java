2711c88d755ca98018cfcf0d2e8bc3434b551426
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-12 12:28:20 +0200

    updated App and KJavaMethod to include javadoc information

--- src/main/java/de/hhu/knife/App.java
+++ src/main/java/de/hhu/knife/App.java
@@ -9,15 +9,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.thoughtworks.qdox.model.*;
 import com.thoughtworks.qdox.JavaProjectBuilder;
+import com.thoughtworks.qdox.model.DocletTag;
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaField;
+import com.thoughtworks.qdox.model.JavaMethod;
+import com.thoughtworks.qdox.model.JavaParameter;
 import com.thoughtworks.qdox.parser.ParseException;
 
-import de.hhu.knife.beans.*;
+import de.hhu.knife.beans.KDocletTag;
+import de.hhu.knife.beans.KJavaClass;
+import de.hhu.knife.beans.KJavaDoc;
+import de.hhu.knife.beans.KJavaField;
+import de.hhu.knife.beans.KJavaMethod;
+import de.hhu.knife.beans.KJavaParameter;
+import de.hhu.knife.beans.Segment;
+import de.hhu.knife.beans.State;
 import de.hhu.knife.transformers.JsonTransformer;
 
 public class App {
@@ -48,22 +58,29 @@ public class App {
 			}
 
 			List<KJavaClass> kJavaClasses = new ArrayList<>();
-			for(JavaClass javaClass : builder.getClasses().stream().collect(Collectors.toList()))
-			{
+			for (JavaClass javaClass : builder.getClasses().stream().collect(Collectors.toList())) {
 				List<KJavaMethod> kJavaMethods = new ArrayList<>();
 				List<KJavaField> kJavaFields = new ArrayList<>();
 				for (JavaMethod javaMethod : javaClass.getMethods()) {
+					String comment = javaMethod.getComment();
+					List<DocletTag> tags = javaMethod.getTags();
+					List<KDocletTag> kDocletTags = new ArrayList<>();
+					for (DocletTag tag : tags) {
+						kDocletTags.add(new KDocletTag.Builder().name(tag.getName()).value(tag.getValue()).build());
+					}
+					KJavaDoc kJavaDoc = new KJavaDoc.Builder().text(comment).tags(kDocletTags).build();
 					List<KJavaParameter> javaParameters = new ArrayList<>();
-					for(JavaParameter javaParameter : javaMethod.getParameters()){
+					for (JavaParameter javaParameter : javaMethod.getParameters()) {
 						javaParameters.add(new KJavaParameter.Builder().parameterInformation(javaParameter).build());
 					}
-					kJavaMethods.add(new KJavaMethod.Builder().methodInformation(javaMethod).parameters(javaParameters).build());
+					kJavaMethods.add(new KJavaMethod.Builder().methodInformation(javaMethod).parameters(javaParameters)
+							.javaDoc(kJavaDoc).build());
 				}
-				for(JavaField javaField : javaClass.getFields())
-				{
+				for (JavaField javaField : javaClass.getFields()) {
 					kJavaFields.add(new KJavaField.Builder().fieldInformation(javaField).build());
 				}
-				kJavaClasses.add(new KJavaClass.Builder().classInformation(javaClass).methods(kJavaMethods).fields(kJavaFields).build());
+				kJavaClasses.add(new KJavaClass.Builder().classInformation(javaClass).methods(kJavaMethods)
+						.fields(kJavaFields).build());
 			}
 			return new Segment.Builder().classes(kJavaClasses).state(State.OK).build();
 		}, new JsonTransformer());
--- src/main/java/de/hhu/knife/beans/KJavaMethod.java
+++ src/main/java/de/hhu/knife/beans/KJavaMethod.java
@@ -7,7 +7,7 @@ import com.thoughtworks.qdox.model.JavaMethod;
 public class KJavaMethod {
 
 	@SuppressWarnings("unused")
-	private List<KJavaParameter> parameters;	
+	private List<KJavaParameter> parameters;
 	@SuppressWarnings("unused")
 	private String codeBlock;
 	@SuppressWarnings("unused")
@@ -17,24 +17,32 @@ public class KJavaMethod {
 	@SuppressWarnings("unused")
 	private boolean isPrivate;
 	@SuppressWarnings("unused")
-	private boolean isPublic;	
+	private boolean isPublic;
 	@SuppressWarnings("unused")
 	private String methodName;
+	@SuppressWarnings("unused")
+	private KJavaDoc javaDoc;
 
 	public static class Builder {
 		private JavaMethod javaMethod;
 		private List<KJavaParameter> parameters;
+		private KJavaDoc javaDoc;
 
-		public Builder methodInformation(JavaMethod javaMethod){
+		public Builder methodInformation(JavaMethod javaMethod) {
 			this.javaMethod = javaMethod;
+			return this;
+		}
+
+		public Builder javaDoc(KJavaDoc kJavaDoc) {
+			this.javaDoc = kJavaDoc;
 			return this;
 		}
 
 		public Builder parameters(List<KJavaParameter> parameters) {
 			this.parameters = parameters;
 			return this;
-		}				
-		
+		}
+
 		public KJavaMethod build() {
 			KJavaMethod kJavaMethod = new KJavaMethod();
 			kJavaMethod.codeBlock = javaMethod.getCodeBlock();
@@ -44,6 +52,7 @@ public class KJavaMethod {
 			kJavaMethod.isPrivate = javaMethod.isPrivate();
 			kJavaMethod.isStatic = javaMethod.isStatic();
 			kJavaMethod.parameters = this.parameters;
+			kJavaMethod.javaDoc = this.javaDoc;
 			return kJavaMethod;
 		}
 	}
