906ce1036ce91aaac4b3f0a1effe9298d1552292
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-12 12:27:06 +0200

    added missing example1.java

---
+++ examples/example1.java
@@ -0,0 +1,44 @@
+package com.hmkcode;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.puppycrawl.tools.checkstyle.gui.Main;
+
+public class A {
+	/**
+	 * Some text
+	 * 
+	 * @param url
+	 *            some url
+	 * @param name
+	 *            some name
+	 * @return the image at the specified URL
+	 * @see Image
+	 */
+	public Image getImage(URL url, String name) {
+		try {
+			return getImage(new URL(url, name));
+		} catch (MalformedURLException e) {
+			return null;
+		}
+	}
+	
+	// This is a comment
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+}
+
+public class B {
+
+	public static Map sortByKey(Map unsortedMap) {
+		Map sortedMap = new TreeMap(); // This is another comment
+		sortedMap.putAll(unsortedMap); 
+		/*
+		 * Also a comment
+		 */
+		return sortedMap;
+	}
+}
