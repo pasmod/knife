4138d1ebd7f4b6add1ae8e6fa9ab2bb93972e1bc
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-14 10:31:31 +0200

    added new mappers to map between JavaClass|KJavaCass and JavaField|KJavaField

--- src/main/java/de/hhu/knife/mappers/Mapper.java
+++ src/main/java/de/hhu/knife/mappers/Mapper.java
@@ -3,11 +3,17 @@ package de.hhu.knife.mappers;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaField;
 import com.thoughtworks.qdox.model.JavaMethod;
 import com.thoughtworks.qdox.model.JavaParameter;
 
+import de.hhu.knife.beans.KJavaClass;
+import de.hhu.knife.beans.KJavaComment;
+import de.hhu.knife.beans.KJavaField;
 import de.hhu.knife.beans.KJavaMethod;
 import de.hhu.knife.beans.KJavaParameter;
+import de.hhu.knife.extractors.Extractor;
 
 public class Mapper {
 
@@ -23,6 +29,21 @@ public class Mapper {
   private static KJavaParameter from(final JavaParameter javaParameter) {
     return new KJavaParameter.Builder().type(javaParameter.getType().getValue())
         .name(javaParameter.getName()).build();
+  }
+
+  public static KJavaClass from(final JavaClass javaClass) {
+    List<KJavaField> kJavaFields = javaClass.getFields().stream().map(jf -> from(jf)).collect(Collectors.toList());
+    List<KJavaMethod> kJavaMethods = javaClass.getMethods().stream().map(jm -> from(jm)).collect(Collectors.toList());
+    List<KJavaComment> kJavaComments = Extractor.extractComments(javaClass);
+    return new KJavaClass.Builder().fields(kJavaFields).methods(kJavaMethods)
+        .packageName(javaClass.getPackageName()).name(javaClass.getName()).comments(kJavaComments)
+        .isPrivate(javaClass.isPrivate()).isStatic(javaClass.isStatic())
+        .isPublic(javaClass.isPublic()).build();
+  }
+
+  public static KJavaField from(final JavaField javaField) {
+    return new KJavaField.Builder().name(javaField.getName()).type(javaField.getType().getName())
+        .codeBlock(javaField.getCodeBlock()).build();
   }
 
 }
