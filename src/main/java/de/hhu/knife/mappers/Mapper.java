8a4635e4bca0f1d1c237af935da82298e3ffa348
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-14 11:18:02 +0200

    added new mapper to convert between Range and KRange

--- src/main/java/de/hhu/knife/mappers/Mapper.java
+++ src/main/java/de/hhu/knife/mappers/Mapper.java
@@ -3,6 +3,7 @@ package de.hhu.knife.mappers;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.github.javaparser.Range;
 import com.thoughtworks.qdox.model.JavaClass;
 import com.thoughtworks.qdox.model.JavaField;
 import com.thoughtworks.qdox.model.JavaMethod;
@@ -13,19 +14,21 @@ import de.hhu.knife.beans.KJavaComment;
 import de.hhu.knife.beans.KJavaField;
 import de.hhu.knife.beans.KJavaMethod;
 import de.hhu.knife.beans.KJavaParameter;
+import de.hhu.knife.beans.KPosition;
+import de.hhu.knife.beans.KRange;
 import de.hhu.knife.extractors.Extractor;
 
 public class Mapper {
 
     public static KJavaClass from(final JavaClass javaClass) {
         final List<KJavaField> kJavaFields = javaClass.getFields()
-                                                .stream()
-                                                .map(jf -> from(jf))
-                                                .collect(Collectors.toList());
+                                                      .stream()
+                                                      .map(jf -> from(jf))
+                                                      .collect(Collectors.toList());
         final List<KJavaMethod> kJavaMethods = javaClass.getMethods()
-                                                  .stream()
-                                                  .map(jm -> from(jm))
-                                                  .collect(Collectors.toList());
+                                                        .stream()
+                                                        .map(jm -> from(jm))
+                                                        .collect(Collectors.toList());
         final List<KJavaComment> kJavaComments = Extractor.extractComments(javaClass);
         return new KJavaClass.Builder().fields(kJavaFields)
                                        .methods(kJavaMethods)
@@ -48,9 +51,9 @@ public class Mapper {
 
     public static KJavaMethod from(final JavaMethod javaMethod) {
         final List<KJavaParameter> kJavaParameters = javaMethod.getParameters()
-                                                         .stream()
-                                                         .map(jp -> from(jp))
-                                                         .collect(Collectors.toList());
+                                                               .stream()
+                                                               .map(jp -> from(jp))
+                                                               .collect(Collectors.toList());
         return new KJavaMethod.Builder().parameters(kJavaParameters)
                                         .codeBlock(javaMethod.getCodeBlock())
                                         .sourceCode(javaMethod.getSourceCode())
@@ -66,6 +69,18 @@ public class Mapper {
                                                               .getValue())
                                            .name(javaParameter.getName())
                                            .build();
+    }
+
+    public static KRange from(final Range range) {
+        final KPosition begin = new KPosition.Builder().line(range.begin.line)
+                                                       .column(range.begin.column)
+                                                       .build();
+        final KPosition end = new KPosition.Builder().line(range.end.line)
+                                                     .column(range.end.column)
+                                                     .build();
+        return new KRange.Builder().begin(begin)
+                                   .end(end)
+                                   .build();
     }
 
 }
