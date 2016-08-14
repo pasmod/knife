cfe3c57139225e6d581140249ae54ac290ed0215
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-14 11:46:56 +0200

    major refactoring of Extractor

--- src/main/java/de/hhu/knife/extractors/Extractor.java
+++ src/main/java/de/hhu/knife/extractors/Extractor.java
@@ -1,47 +1,29 @@
 package de.hhu.knife.extractors;
 
 import java.io.StringReader;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.comments.Comment;
 import com.thoughtworks.qdox.model.JavaClass;
 
 import de.hhu.knife.beans.KJavaComment;
-import de.hhu.knife.beans.Position;
-import de.hhu.knife.beans.Range;
+import de.hhu.knife.mappers.Mapper;
 
 public class Extractor {
-
     public static List<KJavaComment> extractComments(final JavaClass javaClass) {
-        List<Comment> comments = new ArrayList<>();
-        final List<KJavaComment> kJavaComments = new ArrayList<>();
-        CompilationUnit parser;
         try {
-            parser = JavaParser.parse(new StringReader(javaClass.getCodeBlock()));
-            comments = parser.getComments();
-            for (final Comment c : comments) {
-                final Position begin = new Position.Builder().line(c.getRange().begin.line)
-                                                       .column(c.getRange().begin.column)
-                                                       .build();
-                final Position end = new Position.Builder().line(c.getRange().end.line)
-                                                     .column(c.getRange().end.column)
-                                                     .build();
-                kJavaComments.add(new KJavaComment.Builder().content(c.getContent())
-                                                            .type(c.getClass()
-                                                                   .getSimpleName())
-                                                            .range(new Range.Builder().begin(begin)
-                                                                                      .end(end)
-                                                                                      .build())
-                                                            .build());
-            }
-            return kJavaComments;
+            CompilationUnit parser = JavaParser.parse(new StringReader(javaClass.getCodeBlock()));
+            return parser.getComments()
+                         .stream()
+                         .map(c -> Mapper.from(c))
+                         .collect(Collectors.toList());
         } catch (final ParseException e) {
             e.printStackTrace();
         }
-        return null;
+        return Collections.emptyList();
     }
 }
