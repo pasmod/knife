b6dfa8c3486e0bfc3d91909b9ea274bda97c7bd1
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-14 09:53:29 +0200

    slight modification of example1

--- examples/example1.java
+++ examples/example1.java
@@ -1,7 +1,8 @@
 public class Example {
 	int counter = 5;
+
 	/*
-	 * JavaDoc comment
+	 * JavaDoc Comment 1
 	 */
 	public static void main(String[] args) {
 		System.out.println(add(2, 3));
@@ -10,43 +11,38 @@ public class Example {
 	}
 
 	/*
-	 * JavaDoc comment
-	 * this is good
+	 * JavaDoc Comment 2 Second Line
 	 */
 	public static int add(final int a, final int b) {
 		return a + b;
 	}
 
-  public double divide(double a, double b){
-    return 1.0 * a / b; // Line Comment
-  }
+	public double divide(double a, double b) {
+		return 1.0 * a / b; // Line Comment 1
+	}
 
 }
 
 public class Example2 {
-	
 	int counter = 5;
+
 	public static void main(String[] args) {
 		/*
-		 * huhu
-		 * frfrfrfrfr
-		 * rfrfrfr
-		 * rfrfrf
-		 * 
+		 * Block Comment 1 2 2
 		 */
 		System.out.println(2);
 		System.out.println(3);
 	}
 }
 
-
 public class SecondClass {
 
 	public void print(int a) {
 		System.out.println(a);
+		// Line Comment 2
 	}
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println(2);
 	}
 }
