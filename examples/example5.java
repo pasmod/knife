89d0a5bed884231cc968af1ae04e9f3c3a39d951
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-09-13 14:42:11 +0200

    Add another example with variables inside methods

---
+++ examples/example5.java
@@ -0,0 +1,32 @@
+public class example5 {
+    int counter = 5;
+
+    /**
+     * Main method of the example
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+    	int variable1 = 5;
+    	float variable2 = 500;
+
+    }
+
+    public static int add(final int a, final int b) {
+    	int result = a + b;
+        return result;
+    }
+
+    public double divide(double a, double b) {
+        return 1.0 * a / b; // Line Comment 1
+    }
+
+}
+
+public class example51 {
+	public void foo()
+	{
+		int bar = 123;
+	}
+	
+}
